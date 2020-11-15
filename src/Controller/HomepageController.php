fc7ab269e0d8dabed2e9404c5d065b3a2780b05c
spackydev <mathieu.capon@gmail.com>
2020-11-15 23:43:53 +0100

    Integration Form , preparation JS autocompletion

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,17 +4,36 @@ namespace App\Controller;
 
 use App\Form\MerchantType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use function dd;
 
 class HomepageController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $merchantForm = $this->createForm(MerchantType::class);
+
+        $merchantForm->handleRequest ($request);
+
+        if ($merchantForm->isSubmitted () && $merchantForm->isValid ()) {
+            $data = $form->getData ();
+
+            dd($data);
+
+            //$em->persist ($merchant);
+            //$em->flush ();
+
+            //return $this->redirectToRoute ('merchant_add');
+        }
+
+
+
 
         return $this->render('homepage/index.html.twig', [
             'merchantForm' => $merchantForm->createView(),
